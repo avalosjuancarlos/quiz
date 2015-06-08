ce05993193217ac155e431c1ab3e808fe7a80710
Juan Carlos Avalos <avalosjuancarlos@gmail.com>
2015-06-08 15:52:50 -0300

    sequelize y sqlite

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
@@ -12,7 +12,7 @@ router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
 /* GET author */
-router.get('/author', function(req, res){
+router.get('/author', function (req, res) {
   res.render('author');
 });
 
