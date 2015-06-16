784ed632abd7990d099e16d9dc30abccb572ee33
Juan Carlos Avalos <avalosjuancarlos@gmail.com>
2015-06-16 13:36:08 -0300

    agregar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,7 +20,7 @@ exports.index = function(req, res){
 	if( search && search !== "" ) {
 		var searchQ = ("%" + search.trim() + "%").split(" ").join("%");
 		
-		models.Quiz.findAll({where:["lower(pregunta) like lower(?)", searchQ]}).then(function(quizes){
+		models.Quiz.findAll({where:["lower(pregunta) like lower(?)", searchQ], order:[['pregunta', 'ASC']]}).then(function(quizes){
 			res.render('quizes/index.ejs', {quizes: quizes, search: search});
 		});
 	}
@@ -47,4 +47,21 @@ exports.answer = function(req, res){
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{ pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	
+	res.render('quizes/new', {quiz: quiz});
+};
 
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "repuesta"]}).then(function(){
+		res.redirect('/quizes');
+	});	// Redirección HTTP (URL relativo) lista de preguntas
+};
