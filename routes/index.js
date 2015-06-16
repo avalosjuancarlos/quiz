64f96326195ff3e4083908e9aa55199c92b11278
Juan Carlos Avalos <avalosjuancarlos@gmail.com>
2015-06-16 16:06:38 -0300

    se agrego errors al render author

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post('/quizes/create',              quizController.create);
 
 /* GET author */
 router.get('/author', function (req, res) {
-  res.render('author');
+  res.render('author', {errors:[]});
 });
 
 module.exports = router;
