927e3d1f8064b1147a2782bbd0c0348c2a4aaf9d
Juan Carlos Avalos <avalosjuancarlos@gmail.com>
2015-06-17 20:58:25 -0300

    agregar comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-	models.Quiz.find(quizId).then(
-		function(quiz){
+	models.Quiz.find({
+		where: {id: Number(quizId)},
+		include: [{model: models.Comment}]
+	}).then(function(quiz){
 			if (quiz) {
 				req.quiz = quiz;
 				next();
-			} else {
-				next(new Error('No existe quizId=' + quizId));
-			}
+			} else {next(new Error('No existe quizId=' + quizId));}
 		}
 	).catch(function(error){ next(error); });
 };
@@ -34,9 +34,7 @@ exports.index = function(req, res){
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz, errors:[]});
-	});
+	res.render('quizes/show', {quiz: req.quiz, errors:[]}); // req.quiz: instancia de quiz cargada con autoload
 };
 
 // GET /quizes/:id/answer
